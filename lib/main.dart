daade66aa4c0e64b152b3d1759f6b6a48791aa29
Sparrow Paul <sparrowpaul2@gmail.com>
2020-04-06 23:59:05 +0000

    chapter 1

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Awesome App"),
+      ),
+      body: Container(
+        child: Text("Hello Sparrow"),
+      ),
+    );
+  }
+}
