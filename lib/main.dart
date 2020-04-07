6ccb22e321fbc16d371a5a5af0017e0c2b8172ac
Sparrow Paul <sparrowpaul2@gmail.com>
2020-04-07 13:43:49 +0000

    container and text widget

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,34 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        child: Text("Hello Sparrow"),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(colors: [
+                Colors.purple,
+                Colors.blue,
+                Colors.teal,
+              ]),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.blue,
+                  blurRadius: 7,
+                )
+              ]),
+          width: 100,
+          height: 100,
+          child: Text("i am a box", style: TextStyle(
+            fontSize: 20,
+            color: Colors.white,
+            textBaseline: TextBaseline.alphabetic,
+            fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
       ),
     );
   }
